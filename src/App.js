c40270eaac1514d4744d89dd117716087c695a4a
Leah Wilner <leahwilner1@gmail.com>
2023-01-26 19:35:27 -0600

    form working

--- src/App.js
+++ src/App.js
@@ -15,49 +15,68 @@ class App extends React.Component {
     };
   }
 
-  handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      let city = await axios.get("https://us1.locationiq.com/v1/search.php");
-      this.setState({
-        city: city.data.results,
-        error: false,
-      });
-    } catch (error) {
-      this.setState({
-        errorMessage: `An errpr has occurred: ${error.response.status}`,
-      });
-    }
-  };
+  // handleSubmit = async (event) => {
+  //   event.preventDefault();
+  //   try {
+  //     let city = await axios.get("https://us1.locationiq.com/v1/search.php");
+  //     this.setState({
+  //       city: city.data.results,
+  //       error: false,
+  //     });
+  //   } catch (error) {
+  //     this.setState({
+  //       errorMessage: `An errpr has occurred: ${error.response.status}`,
+  //     });
+  //   }
+  // };
+  handleCityInput = (event) => {
+    console.log("🚀 ", event.target.value)
 
+    this.setState({
+      city: event.target.value,
+    });
+
+  }
   submitCityHandler = async (event) => {
     event.preventDefault();
 
-    try {
-      let url = `https://us1.locationiq.com/v1/search?key=${API_KEY}&q=${this.city.state}&format=json`;
 
-      let cityInfo = await axios.get(url);
+  //   try {
+  //     let url = `https://us1.locationiq.com/v1/search?key=${API_KEY}&q=${this.city.state}&format=json`;
 
-      this.setState({
-        cityData: cityInfo.data[0],
-        error: false,
-      });
-    } catch (error) {
-      this.setState({
-        error: true,
-        errorMessage: `An error has occurred: ${error.response.status}`,
-      });
-    }
-  };
+  //     let cityInfo = await axios.get(url);
+
+  //     this.setState({
+  //       cityData: cityInfo.data[0],
+  //       error: false,
+  //     });
+  //   } catch (error) {
+  //     this.setState({
+  //       error: true,
+  //       errorMessage: `An error has occurred: ${error.response.status}`,
+  //     });
+  //   }
+
+  
+  // }  
+};
+  
 
   render() {
-    // let cityList = this.state.city.map((name, index) => {
+    // let city = this.state.city.map((name, index) => {
     //   return <li key={index}>{city.name}</li>;
     // });
 
     return (
       <>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.submitCityHandler}>
+          <label>
+            <input 
+            type="text"
+            onInput={this.handleCityInput}
+            plcaeholder="Enter a City"
+            />
+          </label>
           <button type="submit">Explore!</button>
         </form>
       </>
